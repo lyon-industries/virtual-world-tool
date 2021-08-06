8bc43897cb3e85455eff08e92de5cab161835a41
Christopher Lyon <christopherlyon.cl@gmail.com>
2021-08-06 10:39:51 +0200

    Added side-bar & SAT view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
-function App() {
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2hyaXN0b3BoZXJseW9uIiwiYSI6ImNrcnpnazJ5YzByY3MycW1zMWZpamV3NXgifQ.BpYA5d1avuhfZair0e5dLQ";
+
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(5.7942);
+  const [lat, setLat] = useState(58.8867);
+  const [zoom, setZoom] = useState(15);
+  const [pitch, setPitch] = useState(60);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/satellite-v9",
+      center: [lng, lat],
+      zoom: zoom,
+      pitch: pitch,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
-
-export default App;
